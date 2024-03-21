aa45b66dda6e6432ee6539290103fd33ae3d67ff
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-03-21 11:27:17 -0700

    clean code

--- functions.php
+++ functions.php
@@ -75,17 +75,6 @@ function cascadia_floral_setup()
 		)
 	);
 
-	// Set up the WordPress core custom background feature.
-	// add_theme_support(
-	// 	'custom-background',
-	// 	apply_filters(
-	// 		'cascadia_floral_custom_background_args',
-	// 		array(
-	// 			'default-color' => 'ffffff',
-	// 			'default-image' => '',
-	// 		)
-	// 	)
-	// );
 
 	// Add theme support for selective refresh for widgets.
 	add_theme_support('customize-selective-refresh-widgets');
@@ -125,21 +114,7 @@ add_action('after_setup_theme', 'cascadia_floral_content_width', 0);
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-// function cascadia_floral_widgets_init()
-// {
-// 	register_sidebar(
-// 		array(
-// 			'name' => esc_html__('Sidebar', 'cascadia-floral'),
-// 			'id' => 'sidebar-1',
-// 			'description' => esc_html__('Add widgets here.', 'cascadia-floral'),
-// 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-// 			'after_widget' => '</section>',
-// 			'before_title' => '<h2 class="widget-title">',
-// 			'after_title' => '</h2>',
-// 		)
-// 	);
-// }
-// add_action('widgets_init', 'cascadia_floral_widgets_init');
+
 
 /**
  * Enqueue scripts and styles.
--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -48,19 +48,6 @@ function cascadia_floral_woocommerce_scripts()
 {
 	wp_enqueue_style('cascadia-floral-woocommerce-style', get_template_directory_uri() . '/woocommerce.css', array(), _S_VERSION);
 
-// 	$font_path   = WC()->plugin_url() . '/assets/fonts/';
-// 	$inline_font = '@font-face {
-// 			font-family: "star";
-// 			src: url("' . $font_path . 'star.eot");
-// 			src: url("' . $font_path . 'star.eot?#iefix") format("embedded-opentype"),
-// 				url("' . $font_path . 'star.woff") format("woff"),
-// 				url("' . $font_path . 'star.ttf") format("truetype"),
-// 				url("' . $font_path . 'star.svg#star") format("svg");
-// 			font-weight: normal;
-// 			font-style: normal;
-// 		}';
-
-// 	wp_add_inline_style('cascadia-floral-woocommerce-style', $inline_font);
 }
 add_action('wp_enqueue_scripts', 'cascadia_floral_woocommerce_scripts');
 
