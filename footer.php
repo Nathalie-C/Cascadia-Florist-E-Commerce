c34e32c435d55290c1315ab8204b749de02f2ff2
Nathalie Chang <nathalie.ch.chang@gmail.com>
2024-03-21 14:05:27 -0700

    update footer

--- footer.php
+++ footer.php
@@ -47,17 +47,6 @@ if ($fields) {
 			<a href="<?php the_permalink(497); ?>"><?php echo esc_html(get_the_title(497)); ?></a>
 
 			<p>&#169; 2024 Emily He, Jean Lin, Kaia Sun, Nathalie Chang</p>
-			<a href="<?php echo esc_url(__('https://wordpress.org/', 'cascadia-floral')); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf(esc_html__('Proudly powered by %s', 'cascadia-floral'), 'WordPress');
-				?>
-			</a>
-			<span class="sep"> | </span>
-			<?php
-			/* translators: 1: Theme name, 2: Theme author. */
-			printf(esc_html__('Theme: %1$s by %2$s.', 'cascadia-floral'), 'cascadia-floral', '<a href="https://cascadiafloral.bcitwebdeveloper.ca/">FWD35</a>');
-			?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 <?php } ?>
