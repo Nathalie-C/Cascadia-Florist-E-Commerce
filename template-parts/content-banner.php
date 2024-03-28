2d249ef05dfe7e59097175431b843cd7328d2776
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-03-28 10:53:00 -0700

    update banner image

--- template-parts/content-banner.php
+++ template-parts/content-banner.php
@@ -16,23 +16,24 @@
                     $heading = get_sub_field('heading');
                     $description = get_sub_field('description');
                     $view_all = get_sub_field('view_all');
-					$image = get_sub_field('image');
+					          $image = get_sub_field('image');
                 ?>
                     <div class="banner-content">
-					  <div class="content-background">
+					            <div class="content-background">
                          <?php if ($heading) : ?>
                             <h2><?php echo esc_html($heading); ?></h2>
                          <?php endif; ?>
                          <?php if ($description) : ?>
                             <p><?php echo esc_html($description); ?></p>
                          <?php endif; ?>
-						 <?php if ($view_all && is_array($view_all) && isset($view_all['url'])) : ?>
+						             <?php if ($view_all && is_array($view_all) && isset($view_all['url'])) : ?>
                          <a href="<?php echo esc_url($view_all['url']); ?>">View All</a>
                        <?php endif; ?>
-					  </div>
-					    <?php if ($image) : ?>
-                            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
-                        <?php endif; ?>
+					            </div>
+						            <?php if ($image) : 
+														$image_id = $image['id']; 
+														echo wp_get_attachment_image($image_id, 'full', false, array('alt' => esc_attr($image['alt'])));
+												endif; ?>
                     </div>
                     <?php endwhile; ?>
                 <?php endif; ?>
